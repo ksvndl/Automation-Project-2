687ad37f54f033815df1f6bcd6c8c7f57f7d27d8
ksvndl <kasvandl@gmail.com>
2024-10-28 16:19:44 +0200

    Issue-create assignment complete

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://jira.ivorreic.com/project/board",
+    baseUrl: 'https://jira.ivorreic.com/project/board',
     env: {
-      baseUrl: "https://jira.ivorreic.com/",
+      baseUrl: 'https://jira.ivorreic.com/',
     },
     defaultCommandTimeout: 60000,
     requestTimeout: 60000,
