4734199006d78ed34f590bcbe3b7b22b6c15e58b
ksvndl <kasvandl@gmail.com>
2024-10-28 19:11:16 +0200

    issue-delete assignment

---
+++ cypress/e2e/issue-delete.cy.js
@@ -0,0 +1,35 @@
+describe('Issue delete', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.url()
+      .should('eq', `${Cypress.env('baseUrl')}project`)
+      .then((url) => {
+        cy.visit(url + '/board');
+        cy.contains('This is an issue of type: Task.').click();
+        cy.get('[data-testid="modal:issue-details"]').should('be.visible');
+      });
+  });
+
+  it('Should delete issue and confirm deletion', () => {
+    cy.get('[data-testid="icon:trash"]').should('exist').click();
+    cy.get('[data-testid="modal:confirm"]').should('be.visible');
+    cy.get('button').contains('Delete').should('be.visible').click();
+    cy.get('[data-testid="modal:issue-details"]').should('not.exist');
+    cy.get('[data-testid="modal:confirm"]').should('not.exist');
+    cy.reload();
+    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`);
+    cy.contains('This is an issue of type: Task.').should('not.exist');
+  });
+
+  it('Should initiate deleta and then cancel', () => {
+    cy.get('[data-testid="icon:trash"]').should('exist').click();
+    cy.get('[data-testid="modal:confirm"]').should('be.visible');
+    cy.get('button').contains('Cancel').should('be.visible').click();
+    cy.get('[data-testid="modal:confirm"]').should('not.exist');
+    cy.get('[data-testid="modal:issue-details"]').should('exist');
+    cy.get('[data-testid="icon:close"]').eq(0).should('be.visible').click();
+    cy.reload();
+    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`);
+    cy.contains('This is an issue of type: Task.').should('exist');
+  });
+});
