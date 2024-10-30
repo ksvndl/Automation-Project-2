7770e48cff0e5cda749907e1ab7945ae935e44e0
ksvndl <kasvandl@gmail.com>
2024-10-30 22:49:13 +0200

    changes 2024-10-30

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -12,10 +12,13 @@ describe('Issue delete', () => {
 
   it('Should delete issue and confirm deletion', () => {
     cy.get('[data-testid="icon:trash"]').should('exist').click();
-    cy.get('[data-testid="modal:confirm"]').should('be.visible');
-    cy.get('button').contains('Delete').should('be.visible').click();
-    cy.get('[data-testid="modal:issue-details"]').should('not.exist');
+    cy.get('[data-testid="modal:confirm"]')
+      .should('be.visible')
+      .within(() => {
+        cy.get('button').contains('Delete').should('be.visible').click();
+      });
     cy.get('[data-testid="modal:confirm"]').should('not.exist');
+    cy.get('[data-testid="modal:issue-details"]').should('not.exist');
     cy.reload();
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`);
     cy.contains('This is an issue of type: Task.').should('not.exist');
@@ -23,11 +26,17 @@ describe('Issue delete', () => {
 
   it('Should initiate deleta and then cancel', () => {
     cy.get('[data-testid="icon:trash"]').should('exist').click();
-    cy.get('[data-testid="modal:confirm"]').should('be.visible');
-    cy.get('button').contains('Cancel').should('be.visible').click();
+    cy.get('[data-testid="modal:confirm"]')
+      .should('be.visible')
+      .within(() => {
+        cy.get('button').contains('Cancel').should('be.visible').click();
+      });
     cy.get('[data-testid="modal:confirm"]').should('not.exist');
-    cy.get('[data-testid="modal:issue-details"]').should('exist');
-    cy.get('[data-testid="icon:close"]').eq(0).should('be.visible').click();
+    cy.get('[data-testid="modal:issue-details"]')
+      .should('exist')
+      .within(() => {
+        cy.get('[data-testid="icon:close"]').eq(0).should('be.visible').click();
+      });
     cy.reload();
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`);
     cy.contains('This is an issue of type: Task.').should('exist');
